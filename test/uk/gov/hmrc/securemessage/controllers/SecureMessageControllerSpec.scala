eee003d64f08093114d439ffc9cd63e674ee7950
matthewgroom <matthew.groom@ntlworld.com>
2020-12-10 17:04:26 +0000

    DC-2826: Create CDS conversation API (#4)
    * DC-2912: added wartremover
    
    * DC-2912: bump scoverage version
    
    * DC-2912: added wartremover warnings
    
    * DC-2826: adding create conversation api
    
    * DC-2826: adding create conversation api
    
    * DC-2826: further dev
    
    * DC-2826: further dev
    
    * DC-2826: configuring swagger
    
    * DC-2826: wip
    
    * DC-2826: further dev
    
    * DC-2826: further dev
    
    * DC-2826: further dev
    
    * DC-2826: further dev
    
    * DC-2826: further dev
    
    * DC-2826: further dev
    
    * DC-2826: ignore compiler warnings in generated files
    
    * DC-2826: relocating swagger output location
    
    * DC-2826: further dev
    
    * DC-2826: upgraded to openapi 3.0.3
    
    * DC-2826: further dev
    
    * DC-2826: fixing issues related to openapi 3.0
    
    * DC-2826: further dev
    
    * DC-2826: further dev
    
    * DC-2826: further dev
    
    * DC-2826: further dev
    
    * DC-2826: further dev
    
    * DC-2826: added scoverage to build
    
    * DC-2826: further dev
    
    * DC-2826: furthed dev
    
    * DC-2826: further dev
    
    * DC-2826: further dev
    
    * DC-2826: further dev
    
    * DC-2826: further dev
    
    * DC-2826 add readme table of contents, rename schema file
    
    Co-authored-by: Rob Walpole <2248695+rwalpole@users.noreply.github.com>
    Co-authored-by: Oleg Efrem <oleg.efrem@gmail.com>

---
+++ test/uk/gov/hmrc/securemessage/controllers/SecureMessageControllerSpec.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.securemessage.controllers
+
+import akka.stream.Materializer
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatestplus.play.PlaySpec
+import play.api.http.ContentTypes._
+import play.api.http.HeaderNames._
+import play.api.http.Status._
+import play.api.libs.json.{ JsValue, Json }
+import play.api.test.Helpers.{ PUT, defaultAwaitTimeout, status }
+import play.api.test.{ FakeHeaders, FakeRequest, Helpers, NoMaterializer }
+import uk.gov.hmrc.securemessage.helpers.Resources
+
+class SecureMessageControllerSpec extends PlaySpec with ScalaFutures {
+
+  implicit val mat: Materializer = NoMaterializer
+
+  "Calling createConversation" should {
+    "return CREATED (201) when sent a request with all optional fields populated" in {
+      val fullConversationJson: JsValue = Resources.readJson("FullConversation.json")
+      val controller = new SecureMessageController(Helpers.stubControllerComponents())
+      val fakeRequest = FakeRequest(
+        method = PUT,
+        uri = routes.SecureMessageController.createConversation("cdcm", "123").url,
+        headers = FakeHeaders(Seq(CONTENT_TYPE -> JSON)),
+        body = fullConversationJson
+      )
+      val response = controller.createConversation("cdcm", "123")(fakeRequest)
+      status(response) mustBe CREATED
+    }
+
+    "return CREATED (201) when sent a request with no optional fields populated" in {
+      val minimalConversationJson: JsValue = Resources.readJson("MinimalConversation.json")
+      val controller = new SecureMessageController(Helpers.stubControllerComponents())
+      val fakeRequest = FakeRequest(
+        method = PUT,
+        uri = routes.SecureMessageController.createConversation("cdcm", "123").url,
+        headers = FakeHeaders(Seq(CONTENT_TYPE -> JSON)),
+        body = minimalConversationJson
+      )
+      val response = controller.createConversation("cdcm", "123")(fakeRequest)
+      status(response) mustBe CREATED
+    }
+
+    "return BAD REQUEST (400) when sent a request with required fields missing" in {
+      val controller = new SecureMessageController(Helpers.stubControllerComponents())
+      val fakeRequest = FakeRequest(
+        method = PUT,
+        uri = routes.SecureMessageController.createConversation("cdcm", "123").url,
+        headers = FakeHeaders(Seq(CONTENT_TYPE -> JSON)),
+        body = Json.parse("""{"missing":"data"}""".stripMargin)
+      )
+      val response = controller.createConversation("cdcm", "123")(fakeRequest)
+      status(response) mustBe BAD_REQUEST
+    }
+  }
+}
