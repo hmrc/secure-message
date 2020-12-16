1b619470c2c987afd83a8d906ec2791d16f0dec9
Rob Walpole <2248695+rwalpole@users.noreply.github.com>
2020-12-16 18:10:25 +0000

    DC-3030: creating core conversation API

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,14 +1,30 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import play.core.PlayVersion.current
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc" %% "bootstrap-backend-play-27" % "3.2.0",
-    "uk.gov.hmrc" %% "simple-reactivemongo"      % "7.31.0-play-27",
-    "org.webjars" % "swagger-ui"                 % "3.38.0"
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-27"  % "3.2.0",
+    "uk.gov.hmrc"       %% "simple-reactivemongo"       % "7.31.0-play-27",
+    "org.webjars"       % "swagger-ui"                  % "3.38.0",
+    "com.beachape"      %% "enumeratum-play-json"       % "1.6.1",
+    "com.typesafe.play" %% "play-json-joda"             % "2.9.1"
   )
 
   val test = Seq(
