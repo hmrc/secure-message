12504f9c4f22bdd6d4bafd2ea3984b822ff486dc
Oleg Efrem <oleg.efrem@gmail.com>
2021-04-27 14:55:21 +0300

    DC-3280 refactor error handling and filtering (#84)
    * DC-3280 do not handle Error instances
    
    * DC-3280 move implicit classes to own trait
    
    * DC-3280 move filterEnrollments to implicit class
    
    * DC-3280 refactor filter enrolments
    
    * DC-3280 move filter logic to service
    
    * Replace controller getConversations flatMap with map
    
    * DC-3280 remove unused assignment vals in controller
    
    * DC-3280 bring back explicit error auditing
    
    * DC-3280 change enrolment filters from and to or combination

---
+++ app/uk/gov/hmrc/securemessage/services/ImplicitClassesExtensions.scala
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.securemessage.services
+
+import uk.gov.hmrc.auth.core.Enrolments
+import uk.gov.hmrc.securemessage.ParticipantNotFound
+import uk.gov.hmrc.securemessage.models.core.{ Conversation, CustomerEnrolment, Identifier, Participant }
+
+trait ImplicitClassesExtensions {
+  implicit class EnrolmentsExtensions(enrolments: Enrolments) {
+    def asIdentifiers: Set[Identifier] =
+      for {
+        enr <- enrolments.enrolments
+        id  <- enr.identifiers
+      } yield Identifier(id.key, id.value, Some(enr.key))
+
+    def asCustomerEnrolments: Set[CustomerEnrolment] =
+      for {
+        enr <- enrolments.enrolments
+        id  <- enr.identifiers
+      } yield CustomerEnrolment(enr.key, id.key, id.value)
+
+    def find(enrolmentKey: String, enrolmentName: String): Option[CustomerEnrolment] =
+      for {
+        eoriEnrolment       <- enrolments.getEnrolment(enrolmentKey)
+        enrolmentIdentifier <- eoriEnrolment.getIdentifier(enrolmentName)
+      } yield CustomerEnrolment(eoriEnrolment.key, enrolmentIdentifier.key, enrolmentIdentifier.value)
+
+    @SuppressWarnings(Array("org.wartremover.warts.Option2Iterable"))
+    def filter(enrolmentKeys: Set[String], customerEnrolments: Set[CustomerEnrolment]): Set[CustomerEnrolment] = {
+      val originalEnrolments: Set[CustomerEnrolment] = enrolments.asCustomerEnrolments
+      val enrolmentKeysFiltered =
+        if (enrolmentKeys.isEmpty) {
+          originalEnrolments
+        } else {
+          originalEnrolments.filter(e => enrolmentKeys.contains(e.key))
+        }
+      val customerEnrolmentsFiltered =
+        if (customerEnrolments.isEmpty) {
+          enrolmentKeysFiltered
+        } else {
+          originalEnrolments.intersect(customerEnrolments)
+        }
+      enrolmentKeysFiltered ++ customerEnrolmentsFiltered
+    }
+  }
+
+  implicit class ConversationExtensions(conversation: Conversation) {
+    def participantWith(identifiers: Set[Identifier]): Either[ParticipantNotFound, Participant] =
+      conversation.participants.find(p => identifiers.contains(p.identifier)) match {
+        case Some(participant) => Right(participant)
+        case None =>
+          Left(ParticipantNotFound(
+            s"No participant found for client: ${conversation.client}, conversationId: ${conversation.id}, indentifiers: $identifiers"))
+      }
+  }
+}
