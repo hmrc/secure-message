f6426c26d73aec76b8a7ff22cb069e13265f8a04
matthewgroom <matthew.groom@ntlworld.com>
2021-07-26 13:09:57 +0100

    DC-3586: fixed flexmark dep issue

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -33,14 +33,14 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % "5.7.0"          % Test,
-    "uk.gov.hmrc"            %% "reactivemongo-test"       % "5.0.0-play-28"  % Test,
-    "com.typesafe.play"      %% "play-test"                % current          % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0"          % "test, it",
-    "org.scalatestplus"      %% "mockito-3-4"              % "3.2.9.0"        % "test, it",
-    "org.mockito"            % "mockito-core"              % "3.11.2"         % "test, it",
-    "com.vladsch.flexmark"   % "flexmark-all"              % "0.62.2"         % "test, it",
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % "5.7.0"         % Test,
+    "uk.gov.hmrc"            %% "reactivemongo-test"       % "5.0.0-play-28" % Test,
+    "com.typesafe.play"      %% "play-test"                % current         % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0"         % "test, it",
+    "org.scalatestplus"      %% "mockito-3-4"              % "3.2.9.0"       % "test, it",
+    "org.mockito"            % "mockito-core"              % "3.11.2"        % "test, it",
+    "com.vladsch.flexmark"   % "flexmark-all"              % "0.36.8"        % "test, it",
     "uk.gov.hmrc"            %% "service-integration-test" % "1.1.0-play-28" % "test, it",
-    "org.pegdown"            % "pegdown"                   % "1.6.0"          % "test, it"
+    "org.pegdown"            % "pegdown"                   % "1.6.0"         % "test, it"
   )
 }
