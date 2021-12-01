92c1b73a6b2c28260126afdacecf8d278b872f87
Elie Labeca <1290092+elabeca@users.noreply.github.com>
2021-12-01 09:13:14 +0000

    update dependencies (#123)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -28,8 +28,8 @@ object AppDependencies {
     "org.webjars"       % "swagger-ui"                 % "3.42.0",
     "com.beachape"      %% "enumeratum-play"           % "1.5.17",
     "com.typesafe.play" %% "play-json-joda"            % "2.9.1",
-    "org.typelevel"     %% "cats-core"                 % "2.6.1",
-    "org.jsoup"         % "jsoup"                      % "1.14.1"
+    "org.typelevel"     %% "cats-core"                 % "2.7.0",
+    "org.jsoup"         % "jsoup"                      % "1.14.3"
   )
 
   val test = Seq(
@@ -37,8 +37,8 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "reactivemongo-test"       % "5.0.0-play-28" % Test,
     "com.typesafe.play"      %% "play-test"                % current         % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0"         % "test, it",
-    "org.scalatestplus"      %% "mockito-3-4"              % "3.2.9.0"       % "test, it",
-    "org.mockito"            % "mockito-core"              % "3.11.2"        % "test, it",
+    "org.scalatestplus"      %% "mockito-3-4"              % "3.2.10.0"       % "test, it",
+    "org.mockito"            % "mockito-core"              % "4.1.0"        % "test, it",
     "com.vladsch.flexmark"   % "flexmark-all"              % "0.36.8"        % "test, it",
     "uk.gov.hmrc"            %% "service-integration-test" % "1.1.0-play-28" % "test, it",
     "org.pegdown"            % "pegdown"                   % "1.6.0"         % "test, it"
