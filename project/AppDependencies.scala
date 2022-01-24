b2f5586ab2fceef69ba70a47ea4232f480cbdc7f
matthewgroom <matthew.groom@ntlworld.com>
2022-01-24 14:31:09 +0000

    DC-3918: WIP

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -37,8 +37,8 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "reactivemongo-test"       % "5.0.0-play-28" % Test,
     "com.typesafe.play"      %% "play-test"                % current         % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0"         % "test, it",
-    "org.scalatestplus"      %% "mockito-3-4"              % "3.2.10.0"       % "test, it",
-    "org.mockito"            % "mockito-core"              % "4.2.0"        % "test, it",
+    "org.scalatestplus"      %% "mockito-3-4"              % "3.2.10.0"      % "test, it",
+    "org.mockito"            % "mockito-core"              % "4.3.0"         % "test, it",
     "com.vladsch.flexmark"   % "flexmark-all"              % "0.36.8"        % "test, it",
     "uk.gov.hmrc"            %% "service-integration-test" % "1.1.0-play-28" % "test, it",
     "org.pegdown"            % "pegdown"                   % "1.6.0"         % "test, it"
