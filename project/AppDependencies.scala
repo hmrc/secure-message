5903ef3c69aa39f526c7da88d506b896fb4c783c
Jim Collins <jim.r.collins@gmail.com>
2022-05-26 13:09:36 +0100

    DC-4192 - Fix audit log messages from secure-message (#142)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -21,7 +21,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "5.7.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % "0.62.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % "0.64.0",
     "uk.gov.hmrc"       %% "time"                      % "3.19.0",
     "uk.gov.hmrc"       %% "emailaddress"              % "3.5.0",
     "uk.gov.hmrc"       %% "emailaddress"              % "3.5.0",
@@ -29,16 +29,16 @@ object AppDependencies {
     "com.beachape"      %% "enumeratum-play"           % "1.5.17",
     "com.typesafe.play" %% "play-json-joda"            % "2.9.1",
     "org.typelevel"     %% "cats-core"                 % "2.7.0",
-    "org.jsoup"         % "jsoup"                      % "1.14.3"
+    "org.jsoup"         % "jsoup"                      % "1.15.1"
   )
 
   val test = Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % "5.7.0"         % Test,
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % "0.62.0"        % Test,
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % "0.64.0"        % Test,
     "com.typesafe.play"      %% "play-test"                % current         % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0"         % "test, it",
     "org.scalatestplus"      %% "mockito-3-4"              % "3.2.10.0"      % "test, it",
-    "org.mockito"            % "mockito-core"              % "4.4.0"         % "test, it",
+    "org.mockito"            % "mockito-core"              % "4.5.1"         % "test, it",
     "com.vladsch.flexmark"   % "flexmark-all"              % "0.36.8"        % "test, it",
     "uk.gov.hmrc"            %% "service-integration-test" % "1.1.0-play-28" % "test, it",
     "org.pegdown"            % "pegdown"                   % "1.6.0"         % "test, it"
