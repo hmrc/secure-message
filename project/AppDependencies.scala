2d096ddf56ba4a520ae7be02c6980414cc5120da
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2022-08-09 14:54:11 +0100

    DC-4397 Upgrade sbt-auto-build due to codecs issue

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -20,7 +20,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "6.4.0",
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28" % "5.7.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"        % "0.69.0",
     "uk.gov.hmrc"       %% "time"                      % "3.19.0",
     "uk.gov.hmrc"       %% "emailaddress"              % "3.5.0",
@@ -32,7 +32,7 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % "6.4.0"         % Test,
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"   % "5.7.0"         % Test,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % "0.69.0"        % Test,
     "com.typesafe.play"      %% "play-test"                % current         % Test,
     "org.scalatestplus.play" %% "scalatestplus-play"       % "5.1.0"         % "test, it",
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,6 +1,7 @@
 resolvers += MavenRepository("HMRC-open-artefacts-maven2", "https://open.artefacts.tax.service.gov.uk/maven2")
 resolvers += Resolver.url("HMRC-open-artefacts-ivy2", url("https://open.artefacts.tax.service.gov.uk/ivy2"))(
   Resolver.ivyStylePatterns)
+resolvers += Resolver.typesafeRepo("releases")
 
 addSbtPlugin("uk.gov.hmrc"       % "sbt-auto-build"        % "3.7.0")
 addSbtPlugin("uk.gov.hmrc"       % "sbt-distributables"    % "2.1.0")
@@ -11,4 +12,4 @@ addSbtPlugin("com.lucidchart"    % "sbt-scalafmt"          % "1.16")
 addSbtPlugin("org.scoverage"     % "sbt-scoverage"         % "2.0.0")
 addSbtPlugin("org.scalastyle"    % "scalastyle-sbt-plugin" % "1.0.0")
 addSbtPlugin("uk.gov.hmrc"       % "sbt-bobby"             % "3.3.0")
-addSbtPlugin("com.timushev.sbt"  % "sbt-updates"           % "0.5.1")
+addSbtPlugin("com.timushev.sbt"  % "sbt-updates"           % "0.5.3")
