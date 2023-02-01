700c0775a22707d273d84d5bf7fcabd2447728cf
satyajosyula2020 <satya.josyula@digital.hmrc.gov.uk>
2023-02-01 10:18:14 +0000

    Update MessagesResponseSpec.scala

--- test/uk/gov/hmrc/securemessage/controllers/model/MessagesResponseSpec.scala
+++ test/uk/gov/hmrc/securemessage/controllers/model/MessagesResponseSpec.scala
@@ -1,6 +1,17 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package uk.gov.hmrc.securemessage.controllers.model
@@ -14,7 +25,7 @@ import uk.gov.hmrc.securemessage.helpers.Resources
 import uk.gov.hmrc.securemessage.models.core.Letter
 
 //ToDo Add more tests for items in MessagesResponse
-class MessagesResponseSpec extends PlaySpec {
+class MessagesResponseSpec extends PlaySpec with ApiFormats {
 
   "MessagesResponse" must {
     val objectId = new ObjectId
