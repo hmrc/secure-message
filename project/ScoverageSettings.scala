1491266b4de5d3b6123e2d9de51bb15e4cab8f35
satyajosyula2020 <satya.josyula@digital.hmrc.gov.uk>
2023-03-15 09:36:44 +0000

    Update ScoverageSettings.scala

--- project/ScoverageSettings.scala
+++ project/ScoverageSettings.scala
@@ -22,7 +22,7 @@ object ScoverageSettings {
   def apply(): Seq[Def.Setting[_ >: String with Double with Boolean]] =
     Seq( // Semicolon-separated list of regexes matching classes to exclude
       ScoverageKeys.coverageExcludedPackages := "<empty>;.*Reverse.*;.*(config|testonly).*;.*(BuildInfo|Routes).*",
-      ScoverageKeys.coverageMinimumStmtTotal := 62.20,
+      ScoverageKeys.coverageMinimumStmtTotal := 61.00,
       ScoverageKeys.coverageFailOnMinimum := true,
       ScoverageKeys.coverageHighlighting := true,
       ConfigKey.configurationToKey(Test) / parallelExecution := false
