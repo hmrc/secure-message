d8ddf416b24e467aa5787e7a1cbaac1f439624bc
satyajosyula2020 <satya.josyula@digital.hmrc.gov.uk>
2023-05-19 16:32:26 +0100

    Update AppDependencies.scala

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -39,7 +39,7 @@ object AppDependencies {
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28"  % "0.74.0"         % "test, it",
     "org.scalatestplus"      %% "mockito-3-4"              % "3.2.10.0"       % "test, it",
     "uk.gov.hmrc"            %% "service-integration-test" % "1.3.0-play-28"  % "test, it",
-    "uk.gov.hmrc"            %% "dc-message-library"       % "0.29.0"         % "test, it",
+    "uk.gov.hmrc"            %% "dc-message-library"       % "0.30.0"         % "test, it",
     "com.vladsch.flexmark"   %  "flexmark-all"             % "0.36.8"         % "test, it",
     "org.pegdown"            %  "pegdown"                  % "1.6.0"          % "test, it"
   )
