aeb1bbf9f970f2f8e62acc0e3f89c69a2cfbedc0
Murali raju <17339891+muralishub@users.noreply.github.com>
2024-09-17 16:48:10 +0100

    DC-6706: temp job to log user message to splunk

---
+++ app/uk/gov/hmrc/securemessage/repository/TempRepository.scala
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.securemessage.repository
+
+import org.mongodb.scala.ObservableFuture
+import org.mongodb.scala.model.{ Filters, IndexModel }
+import play.api.Logging
+import play.api.libs.json.{ Format, JsValue }
+import uk.gov.hmrc.mongo.MongoComponent
+import uk.gov.hmrc.mongo.play.json.PlayMongoRepository
+import uk.gov.hmrc.securemessage.models.core.Identifier
+
+import javax.inject.{ Inject, Singleton }
+import scala.concurrent.{ ExecutionContext, Future }
+
+@Singleton
+class TempRepository @Inject() (mongo: MongoComponent)(implicit executionContext: ExecutionContext)
+    extends PlayMongoRepository[JsValue](
+      mongo,
+      "message",
+      implicitly[Format[JsValue]],
+      Seq.empty[IndexModel]
+    ) with Logging {
+  def getLettersTempFunc(identifiers: Set[Identifier]): Future[Seq[JsValue]] = {
+    val query = Filters.and(
+      Filters.eq("recipient.identifier.name", identifiers.head.name),
+      Filters.eq("recipient.identifier.value", identifiers.head.value)
+    )
+    collection
+      .find(query)
+      .toFuture()
+      .recover { e =>
+        logger.error(s"Failed to fetch document ${e.getMessage} ")
+        Seq.empty[JsValue]
+      }
+  }
+}
