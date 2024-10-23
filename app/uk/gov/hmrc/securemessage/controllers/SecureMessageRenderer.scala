042e5492367f7902583579849ef6268c4788742f
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2024-10-23 17:11:23 +0100

    DC-6794 Add message renderer endpoint for two-way-message

--- app/uk/gov/hmrc/securemessage/controllers/SecureMessageRenderer.scala
+++ app/uk/gov/hmrc/securemessage/controllers/SecureMessageRenderer.scala
@@ -48,14 +48,12 @@ class SecureMessageRenderer @Inject() (
 
   def view(id: ObjectId): Action[AnyContent] = Action.async { implicit request =>
     implicit val hc: HeaderCarrier = HeaderCarrierConverter.fromRequest(request)
-    println(id)
     authorised() {
-      println(id)
       messageService.getLetter(id) map {
         case Some(letter) if letter.alertDetails.templateId == ATS_v2_renderTemplateId =>
           Ok(templates.html.ViewTaxSummary_v2(letter.subject, letter.validFrom))
             .withHeaders("X-Title" -> UriEncoding.encodePathSegment(letter.subject, "UTF-8"))
-        case r => println(r); InternalServerError
+        case r => InternalServerError
       }
     }
   }
