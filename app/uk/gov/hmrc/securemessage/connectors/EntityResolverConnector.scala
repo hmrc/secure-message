a9e73a2b21a0c68d8b991eb6b06c4724b12aa5e2
satyajosyula2020 <satya.josyula@digital.hmrc.gov.uk>
2024-12-18 17:22:21 +0000

    DC-6851 - Provide flexible GET verified-email-address

--- app/uk/gov/hmrc/securemessage/connectors/EntityResolverConnector.scala
+++ app/uk/gov/hmrc/securemessage/connectors/EntityResolverConnector.scala
@@ -28,59 +28,13 @@ import uk.gov.hmrc.http.client.HttpClientV2
 
 import javax.inject.{ Inject, Singleton }
 import scala.concurrent.{ ExecutionContext, Future }
+
 import java.net.URL
-
-sealed trait VerifiedEmailAddressResponse extends Product with Serializable {
-  def fold[X](validated: String => X, notValidated: String => X): X =
-    this match {
-      case EmailValidation(email)       => validated(email)
-      case x @ VerifiedEmailNotFound(_) => notValidated(x.getMessage)
-    }
-
-  def value: Either[VerifiedEmailNotFound, EmailValidation] =
-    this match {
-      case valid: EmailValidation          => Right(valid)
-      case notFound: VerifiedEmailNotFound => Left(notFound)
-    }
-}
-
-case class EmailValidation(email: String) extends VerifiedEmailAddressResponse
-
-object EmailValidation {
-  implicit val format: OFormat[EmailValidation] = Json.format[EmailValidation]
-}
-
-case class VerifiedEmailNotFound(reasonCode: String) extends VerifiedEmailAddressResponse {
-  def getMessage: String = reasonCode match {
-    case "EMAIL_ADDRESS_NOT_VERIFIED" =>
-      "The backend has rejected the message due to not being able to verify the email address."
-    case "NOT_OPTED_IN"          => "email: not verified as user not opted in"
-    case "PREFERENCES_NOT_FOUND" => "email: not verified as preferences not found"
-    case _                       => "email: not verified for unknown reason"
-  }
-}
-
-case class OtherException(message: String) extends Exception(message)
-
-case class CallFailedException(message: String) extends Exception(message)
-
 @Singleton
 class EntityResolverConnector @Inject() (config: Configuration, httpClient: HttpClientV2)(implicit ec: ExecutionContext)
     extends ServicesConfig(config) with Logging {
 
   def prepareUrl(path: String): URL = common.url(s"${baseUrl("entity-resolver")}$path")
-
-  def verifiedEmailAddress(recipient: TaxEntity)(implicit hc: HeaderCarrier): Future[VerifiedEmailAddressResponse] =
-    httpClient
-      .get(prepareUrl(s"/portal/preferences/${recipient.regime}/${recipient.identifier.value}/verified-email-address"))
-      .execute[HttpResponse]
-      .map { response =>
-        response.status match {
-          case Status.OK        => response.json.as[EmailValidation]
-          case Status.NOT_FOUND => VerifiedEmailNotFound((response.json \ "reason").as[String])
-          case status           => throw new OtherException(s"OTHER_EXCEPTION_$status")
-        }
-      }
 
   def getTaxId(recipient: TaxEntity)(implicit hc: HeaderCarrier): Future[Option[TaxId]] = {
     val allowedRegimes = Set(Regime.itsa, Regime.paye, Regime.sa)
