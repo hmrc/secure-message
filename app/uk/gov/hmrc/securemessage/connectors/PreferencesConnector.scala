80ffb66e4b030c780f01a58e48e6081889419747
Murali raju <17339891+muralishub@users.noreply.github.com>
2025-02-25 10:42:52 +0000

    DC-7167: change preferences/verify/email-address endpoint (#268)

--- app/uk/gov/hmrc/securemessage/connectors/PreferencesConnector.scala
+++ app/uk/gov/hmrc/securemessage/connectors/PreferencesConnector.scala
@@ -74,7 +74,7 @@ class PreferencesConnector @Inject() (config: Configuration, httpClient: HttpCli
     httpClient
       .get(
         prepareUrl(
-          s"/preferences/verify/email-address?regime=${recipient.regime}&taxId=${recipient.identifier.value}"
+          s"/preferences/verified-email?regime=${recipient.regime}&taxId=${recipient.identifier.value}"
         )
       )
       .execute[HttpResponse]
