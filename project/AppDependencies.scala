d66890b66a59c0535cfd52a5a956b508c663020f
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2025-08-20 17:32:25 +0100

    DC-7693 Update dc-message-library version to support new ITSA formIds (#298)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -20,7 +20,7 @@ import sbt._
 object AppDependencies {
 
   private val bootstrapVersion = "10.1.0"
-  private val dcMessageLibraryVersion = "1.15.0"
+  private val dcMessageLibraryVersion = "1.16.0"
 
   val compile = Seq(
     caffeine,
