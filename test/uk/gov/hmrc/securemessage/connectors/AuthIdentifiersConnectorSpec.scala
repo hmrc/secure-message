b3eea6a66256659aa85a0847caa245af1478fde0
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2025-08-28 12:06:25 +0100

    DC-7442 Add support for new enrolment identifier HMRC-IOSS-NETP (#299)

--- test/uk/gov/hmrc/securemessage/connectors/AuthIdentifiersConnectorSpec.scala
+++ test/uk/gov/hmrc/securemessage/connectors/AuthIdentifiersConnectorSpec.scala
@@ -25,7 +25,7 @@ import play.api.Application
 import play.api.http.{ HeaderNames, Status }
 import play.api.inject.*
 import play.api.inject.guice.GuiceApplicationBuilder
-import uk.gov.hmrc.common.message.model.TaxEntity.{ HmceVatdecOrg, HmrcAdOrg, HmrcCusOrg, HmrcIossInt, HmrcIossOrg, HmrcOssOrg, HmrcPodsOrg, HmrcPodsPpOrg, HmrcPptOrg }
+import uk.gov.hmrc.common.message.model.TaxEntity.*
 import uk.gov.hmrc.domain.*
 import uk.gov.hmrc.http.{ Authorization, HeaderCarrier, UpstreamErrorResponse }
 import uk.gov.hmrc.mongo.metrix.MetricOrchestrator
@@ -690,6 +690,42 @@ class AuthIdentifiersConnectorSpec
 
       authConnector.currentEffectiveTaxIdentifiers.futureValue must be(
         Set(HmrcIossInt("IN9001234567"))
+      )
+    }
+
+    "get the tax ids for only HMRC-IOSS-NETP enrolment " in new TestCase {
+
+      val responseBody =
+        """
+          |{
+          |  "allEnrolments": [
+          |    {
+          |      "key": "HMRC-IOSS-NETP",
+          |      "identifiers": [
+          |        {
+          |          "key": "IOSSNumber",
+          |          "value": "IM9001234567"
+          |        }
+          |      ],
+          |      "state": "Activated",
+          |      "confidenceLevel": 200
+          |    }
+          |  ]
+          |}
+                                 """.stripMargin
+
+      givenThat(
+        post(urlEqualTo("/auth/authorise"))
+          .withHeader(HeaderNames.AUTHORIZATION, equalTo(authToken))
+          .willReturn(
+            aResponse()
+              .withStatus(Status.OK)
+              .withBody(responseBody)
+          )
+      )
+
+      authConnector.currentEffectiveTaxIdentifiers.futureValue must be(
+        Set(HmrcIossNetp("IM9001234567"))
       )
     }
 
