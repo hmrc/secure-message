dcefda54a1506bf8217199039b3cda9fc67b3d9c
tyagikumarvineet <22053543+tyagikumarvineet@users.noreply.github.com>
2026-01-04 17:01:14 +0000

    [DC-8289] Ignore v1.template files for test coverage

--- project/ScoverageSettings.scala
+++ project/ScoverageSettings.scala
@@ -30,6 +30,7 @@ object ScoverageSettings {
           |.*controllers\.javascript.*;
           |uk\.gov\.hmrc\.securemessage\.testonly\.controllers.*
   """.stripMargin.replaceAll("\\s+", ""),
+      ScoverageKeys.coverageExcludedFiles := """.*v1.template.*""".stripMargin.replaceAll("\\s+", ""),
       ScoverageKeys.coverageMinimumStmtTotal := 90,
       ScoverageKeys.coverageFailOnMinimum := true,
       ScoverageKeys.coverageHighlighting := true,
