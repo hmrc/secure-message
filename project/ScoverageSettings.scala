f2a670cb4ab4a9a00b6d90931c531e0803a99f21
tyagikumarvineet <22053543+tyagikumarvineet@users.noreply.github.com>
2026-01-05 00:34:22 +0000

    [DC-8289] Add test classes

--- project/ScoverageSettings.scala
+++ project/ScoverageSettings.scala
@@ -30,7 +30,8 @@ object ScoverageSettings {
           |.*controllers\.javascript.*;
           |uk\.gov\.hmrc\.securemessage\.testonly\.controllers.*
   """.stripMargin.replaceAll("\\s+", ""),
-      ScoverageKeys.coverageExcludedFiles := """.*v1.template.*""".stripMargin.replaceAll("\\s+", ""),
+      ScoverageKeys.coverageExcludedFiles := """.*v1.template.*|.*v3.template.*""".stripMargin
+        .replaceAll("\\s+", ""),
       ScoverageKeys.coverageMinimumStmtTotal := 90,
       ScoverageKeys.coverageFailOnMinimum := true,
       ScoverageKeys.coverageHighlighting := true,
