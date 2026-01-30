06be4a57f4b1d5df52a296e6c92898c7acb0bcf6
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2026-01-30 13:09:56 +0000

    DC-8418 Update designatory-details taxpayer SA api to use hip

--- app/uk/gov/hmrc/securemessage/connectors/TaxpayerNameConnector.scala
+++ app/uk/gov/hmrc/securemessage/connectors/TaxpayerNameConnector.scala
@@ -18,16 +18,18 @@ package uk.gov.hmrc.securemessage.connectors
 
 import play.api.Logging
 import play.api.libs.json.*
+import play.api.http.HeaderNames.*
 import uk.gov.hmrc.common.message.model.TaxpayerName
 import uk.gov.hmrc.domain.SaUtr
-import uk.gov.hmrc.http.{ HeaderCarrier, NotFoundException }
+import uk.gov.hmrc.http.{ HeaderCarrier, NotFoundException, StringContextOps }
 import uk.gov.hmrc.play.bootstrap.config.ServicesConfig
 import uk.gov.hmrc.http.HttpReads.Implicits.*
-import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.client.{ HttpClientV2, RequestBuilder }
 
 import javax.inject.{ Inject, Singleton }
 import scala.concurrent.{ ExecutionContext, Future }
 import java.net.URL
+import java.util.{ Base64, UUID }
 
 @Singleton
 class TaxpayerNameConnector @Inject() (
@@ -36,16 +38,22 @@ class TaxpayerNameConnector @Inject() (
 )(implicit ec: ExecutionContext)
     extends Logging {
 
-  def baseUrl: String = servicesConfig.baseUrl("taxpayer-data")
-
-  def prepareUrl(path: String): URL = common.url(s"$baseUrl$path")
+  val serviceUrl: String = servicesConfig.baseUrl("taxpayer-data")
 
   def taxpayerName(utr: SaUtr)(implicit hc: HeaderCarrier): Future[Option[TaxpayerName]] = {
 
     implicit val nameFormat: OFormat[NameFromHods] = NameFromHods.format
 
-    http
-      .get(prepareUrl(s"/self-assessment/individual/$utr/designatory-details/taxpayer"))
+    val taxPayerClient: RequestBuilder =
+      if (hipSwitchOn)
+        http
+          .get(url"$serviceUrlViaHip/ods-sa/v1/self-assessment/individual/$utr/designatory-details/taxpayer")
+          .setHeader(requestHeaders: _*)
+      else
+        http
+          .get(url"$serviceUrl/self-assessment/individual/$utr/designatory-details/taxpayer")
+
+    taxPayerClient
       .execute[NameFromHods]
       .map(_.name)
       .recover {
@@ -56,6 +64,16 @@ class TaxpayerNameConnector @Inject() (
           logger.error(s"Unable to get taxpayer name for $utr, ${e.getMessage}")
           None
       }
+  }
+
+  private val hipSwitchOn: Boolean = servicesConfig.getConfBool("taxpayer-data-hip.enabled", false)
+  private val serviceUrlViaHip: String = servicesConfig.baseUrl("taxpayer-data-hip")
+  private val requestHeaders = {
+    val clientId = servicesConfig.getConfString("taxpayer-data-hip.client-id", "unknown")
+    val clientSecret = servicesConfig.getConfString("taxpayer-data-hip.client-secret", "unknown")
+    val credentials = s"$clientId:$clientSecret"
+    val b64Encoded = Base64.getEncoder.encodeToString(credentials.getBytes("UTF-8"))
+    Seq(AUTHORIZATION -> s"Basic $b64Encoded", "correlationId" -> UUID.randomUUID.toString)
   }
 }
 
