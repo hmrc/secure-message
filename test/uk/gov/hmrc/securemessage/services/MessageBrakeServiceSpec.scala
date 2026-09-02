f3dccd4a3f2e39b8bd85d3686f9ba7a19346d3db
Tarek Ahmed <173894783+TarekAhmed-MG@users.noreply.github.com>
2026-09-02 17:24:48 +0100

    Enable Child Benefit new-service message alerts (CH(A)1700/1708 + Welsh) end-to-end (#330)
    * DC-9325: add formids to messageBreakAllowList
    
    * DC-9352: add formids to messageBreakAllowList
    
    * DC-9352: fix test position

--- test/uk/gov/hmrc/securemessage/services/MessageBrakeServiceSpec.scala
+++ test/uk/gov/hmrc/securemessage/services/MessageBrakeServiceSpec.scala
@@ -213,6 +213,12 @@ class MessageBrakeServiceSpec
         }
       }
     }
+
+    "MessageBrakeAllowList.default" must {
+      "contain the Child Benefit form ids" in {
+        defaultAllowlist must contain allOf ("CH(A)1700", "CH(A)1708")
+      }
+    }
   }
 
   trait TestCase {
